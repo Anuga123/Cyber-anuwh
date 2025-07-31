e96e207e931f25524dcad7df09a2599f5f887885
Anuga Senithu De Silva <anjanee.uragaha@gmail.com>
2025-07-31 16:26:18 +0530

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Services />
+      <Footer />
+    </>
+  )
+}
+
+export default App
